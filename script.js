6bcd756a0f89e3942f8cfaadee019ddc7fe708cb
Zhephyr <ilhamsyawal87@gmail.com>
2023-06-05 10:27:24 +0700

    test_technical_code

---
+++ script.js
@@ -0,0 +1,7 @@
+const segitiga = document.querySelector(".segitiga");
+const ganjil = document.querySelector(".ganjil");
+const prima = document.querySelector(".prima");
+
+segitiga.addEventListener("click", function () {
+  alert("tes");
+});
